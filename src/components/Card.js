41010b617457b37b030644a1841ed81c1ff878d9
Jono Lane <lane7520@gmail.com>
2023-11-07 15:36:32 -0600

    album grid working, trying to fix navbar and footer bleed

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,11 @@ import React from 'react';
 export default function Card({ imageSrc, title }) {
   return (
     <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-      <img src={imageSrc} alt={title} className="w-full h-96 object-cover" />
+      <img
+        src={imageSrc}
+        alt={title}
+        className="w-full h-96 aspect-w-1 aspect-h-1 object-cover"
+      />
       <div className="p-4">
         <h2 className="text-lg font-bold mb-2">{title}</h2>
       </div>
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,13 +11,13 @@ export default function Layout({ children }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Kalam&display=swap" rel="stylesheet" />
         {/* Unifraktur */}
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-          <link href="https://fonts.googleapis.com/css2?family=Kalam&family=UnifrakturMaguntia&display=swap" rel="stylesheet" />
-          {/* Poppins */}
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link href="https://fonts.googleapis.com/css2?family=Kalam&family=UnifrakturMaguntia&display=swap" rel="stylesheet" />
+        {/* Poppins */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet" />
       </head>
       <Navbar />
       <div className="content">
