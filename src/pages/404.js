e6e6bff7405f82ba48e3e9c83c72e40427218974
Jono Lane <lane7520@gmail.com>
2023-12-01 23:32:38 -0600

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,25 @@
 import React from 'react'
-import Layout from '../components/Layout'
 
 const NotFound = () => {
   return (
-    <Layout>
-      <div className='bg-black w-full h-full flex flex-col items-center justify-center'>
-        <h1 className='font-uni tracking-widest'>404</h1>
+      <div className='bg-white min-h-screen flex items-center justify-center'>
+      {Array.from({ length: 50 }, (_, index) => (
+        <h1
+          key={index}
+          className="text-black text-9xl absolute animate-pulse"
+          style={{
+            top: `${Math.random() * 100}vh`,
+            left: `${Math.random() * 100}vw`,
+            animationDelay: `${index * 0.1}s`,
+            animationDuration: '0.8s',
+          }}
+        >
+          404
+        </h1>
+      ))}
       </div>
-    </Layout>
   );
 }
  
 export default NotFound
+
