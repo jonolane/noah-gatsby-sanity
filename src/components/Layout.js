6965df7af4bd750541967c96cbf90810c3df6fcb
jonolane <lane7520@gmail.com>
2023-12-13 12:27:39 -0600

    gatsby head api for link ref

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -154,34 +154,38 @@ export default function Layout({ children }) {
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowFooter(true);
-    }, 800); 
+    }, 800);
 
     return () => clearTimeout(timer);
   }, []);
 
   return (
     <div className="layout bg-white text-black">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link href="https://fonts.googleapis.com/css2?family=Kalam&display=swap" rel="stylesheet" />
-
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link href="https://fonts.googleapis.com/css2?family=Kalam&family=UnifrakturMaguntia&display=swap" rel="stylesheet" />
-
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet" />
-      </head>
       <Navbar />
       <div className="content">
         {children}
       </div>
       {showFooter && <Footer />}
+      <Head />
     </div>
   );
 }
+
+export const Head = () => (
+  <>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Kalam&display=swap" rel="stylesheet" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Kalam&family=UnifrakturMaguntia&display=swap" rel="stylesheet" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet" />
+  </>
+)
 
 // trying to get the page to render only after it's finished loading
 /*
